419f537dcf67fbdc7b1a1048d24d5472e9bcebe3
faZevoLv <jordanfaz95@googlemail.com>
2015-03-06 10:00:57 +0000

    Prebuilt chromium

--- envsetup.sh
+++ envsetup.sh
@@ -634,6 +634,8 @@ function lunch()
 
     echo
 
+    chromium_prebuilt
+
     fixup_common_out_dir
 
     set_stuff_for_environment
@@ -2161,6 +2163,20 @@ function make()
     echo -e " ####"
     echo
     return $ret
+}
+
+function chromium_prebuilt() {
+    T=$(gettop)
+    export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
+    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
+
+    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ]; then
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
+        echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
+    else
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
+        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
+    fi
 }
 
 if [ "x$SHELL" != "x/bin/bash" ]; then
